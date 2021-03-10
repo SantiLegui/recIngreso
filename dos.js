5cb56f183ecab05caf324a2fa78099099444008c
SantiLegui <santilegui.sln@gmail.com>
2021-03-10 10:24:46 -0300

    Examen_terminado

--- dos.js
+++ dos.js
@@ -1,4 +1,152 @@
-function mostrar()
-{
-    alert("Ejercicio 2");
+function mostrar() {
+    /*Realizar el algoritmo que permita ingresar los datos de los 500 alumnos de la UTN FRA, 
+    los datos solicitados son:
+    nombre 
+    carrera("Quimica";"Fisica";"Sistemas")
+    sexo (masculino - femenino - no binario)
+    cantidad de materias(entre 1 y 5)
+    Nota promedio (entre 0 y 10)
+    edad (validar)
+    se debe informar de existir, o informar que no existe , en el caso que corresponda.
+    a) El nombre del mejor promedio de los alumnos que estudian Fisica. (Sin importar el sexo).
+    b) El nombre de la alumna mas joven.
+    c) Porcentaje de estudiantes que estudia cada carrera.
+    d) La edad y nombre del estudiante que cursa mas materias exceptuando la carrera de Quimica.*/
+
+
+    let nombre;
+    let carrera;
+    let sexo;
+    let cantidadMaterias;
+    let nota;
+    let edad;
+    let mejorPromedioFisica;
+    let nombreMejorPromedioFisica;
+    let flagPromedioFisica = 1;
+    let nombreAlumnaJoven;
+    let edadAlumnaJoven;
+    let flagAlumnaJoven = 1;
+    let edadMasMaterias;
+    let nombreMaxCantidadDeMaterias;
+    let maxCantidadDeMaterias;
+    let flagMaxCantidadDeMaterias = 1;
+
+
+
+    for (let i = 0; i < 2; i++) {
+
+        nombre = prompt("Ingrese nombre: ");
+        while (!(isNaN(nombre))) {
+
+
+            nombre = prompt("Error. Ingrese nombre: ");
+
+        }
+
+        carrera = prompt("Ingrese carrera (quimica, fisica o sistemas) : ").toLowerCase();
+        while (carrera != "quimica" && carrera != "fisica" && carrera != "sistemas") {
+
+            carrera = prompt("Error, ingrese carrera (quimica, fisica o sistemas) : ").toLowerCase();
+
+        }
+
+        sexo = prompt("Ingrese sexo (masculino, femenino o no binario) : ");
+        while (sexo != "masculino" && sexo != "femenino" && sexo != "no binario") {
+
+            sexo = prompt("Error, ingrese sexo (masculino, femenino o no binario) : ");
+
+
+        }
+
+        cantidadMaterias = parseInt(prompt("Ingrese cantidad de materias (entre 1 y 5)"));
+        while (cantidadMaterias < 1 || cantidadMaterias > 5 || isNaN(cantidadMaterias)) {
+
+            cantidadMaterias = parseInt(prompt("Error, ingrese cantidad de materias (entre 1 y 5)"));
+
+
+        }
+
+        nota = parseInt(prompt("Ingrese nota (entre 0 y 10)"));
+        while (nota < 0 || nota > 10 || isNaN(nota)) {
+
+            nota = parseInt(prompt("Error, ingrese nota (entre 0 y 10)"));
+
+        }
+
+
+
+        edad = parseInt(prompt("Ingrese edad: "));
+        while (isNaN(edad) || edad <= 17) {
+
+            edad = parseInt(prompt("Error, Ingrese edad: "));
+
+        }
+
+
+
+        if (carrera == "fisica") {
+
+            if (flagPromedioFisica || nota > mejorPromedioFisica) {
+
+                mejorPromedioFisica = nota;
+                nombreMejorPromedioFisica = nombre;
+                flagPromedioFisica = 0;
+
+            }
+        }
+
+
+
+
+        if (sexo == "femenino") {
+
+            if (flagAlumnaJoven || edad < edadAlumnaJoven) {
+
+                edadAlumnaJoven = edad;
+                nombreAlumnaJoven = nombre;
+                flagAlumnaJoven = 0;
+
+            }
+        }
+
+
+
+        if (carrera != "quimica") {
+
+            if (flagMaxCantidadDeMaterias || cantidadMaterias > maxCantidadDeMaterias) {
+
+                maxCantidadDeMaterias = cantidadMaterias;
+                nombreMaxCantidadDeMaterias = nombre;
+                edadMasMaterias = edad;
+                flagMaxCantidadDeMaterias = 0;
+
+            }
+
+        }
+
+    }
+
+
+    if (flagPromedioFisica != 0) {
+        console.log("a) No se ingresaron alumnos que estudien fisica")
+    }
+    else {
+        console.log("a) El nombre del mejor promedio de los alumnos que estudian Fisica es: " + nombreMejorPromedioFisica + " con un promedio de " + mejorPromedioFisica);
+    }
+    if (flagAlumnaJoven != 0) {
+        console.log("b) No se ingresaron alumnas");
+    }
+    else {
+        console.log("b) El nombre de la alumna mas joven es: " + nombreAlumnaJoven + " con una edad de " + edadAlumnaJoven);
+    }
+    if (flagMasMaterias != 0) {
+        console.log("Sólo se ingresaron alumnos que cursan química")
+    }
+    else {
+        console.log("La edad y nombre del estudiante que cursa mas materias es: " + edadMasMaterias + " y " + nombreMaxCantidadDeMaterias + " con una cantidad de materias de " + maxCantidadDeMaterias);
+    }
+
+
+
+
 }
--- uno.js
+++ uno.js
@@ -1,5 +1,132 @@
+/*Una software factory registra la siguiente informacion de sus empleados:
+Nombre ,edad (validar)
+sexo (masculino - femenino - no binario)
+puesto (programador - analista - Qa)
+sueldo (entre 15000 y 70000)
+La empresa desea saber: se debe informar de existir, o informar que no existe , en el caso que corresponda.
+a) promedio de sueldos para cada puesto
+b) el sexo del que percibe el mayor sueldo
+c) el nombre del empleado femenino con mas sueldo
+d) cantidad de programadores no binarios que cobran sueldos entre 20000 y 55000 
+*/
+function mostrar() {
+	let nombre;
+	let edad;
+	let sexo;
+	let sueldoF = 0;
+	let sueldoM = 0;
+	let sueldoNB = 0;
+	let puesto;
+	let sueldo;
+	let promProgramador = 0;
+	let acumProgramador = 0;
+	let contProgramador = 0;
+	let promAnalista = 0;
+	let acumAnalista = 0;
+	let contAnalista = 0;
+	let promQa = 0;
+	let acumQa = 0;
+	let contQa = 0;
+	let sueldoMax;
+	let flagFemSueldo = 1;
+	let femSueldo;
+	let nombreFemMaxSueldo;
+	let programNoBin = 0;
+	let seguir = "s";
 
-function mostrar()
-{
-	alert("Ejercicio 1");
+
+	do {
+		nombre = prompt("Ingrese un nombre: ");
+
+		edad = parseInt(prompt("Ingrese la edad: "));
+		while (isNaN(edad) || edad <= 0) {
+			edad = parseInt(prompt("Error. Ingrese la edad: "));
+		}
+		sexo = prompt("Ingrese sexo: femenino/masculino/no binario").toLowerCase();
+		while (sexo != "femenino" && sexo != "masculino" && sexo != "no binario") {
+			sexo = prompt("Error. Ingrese sexo: femenino/masculino/no binario").toLowerCase();
+		}
+		puesto = prompt("Ingrese el puesto:  programador/analista/Qa").toLowerCase();
+		while (puesto != "programador" && puesto != "analista" && puesto != "qa") {
+			puesto = prompt("error. Ingrese el puesto:  programador/analista/Qa").toLowerCase();
+		}
+		sueldo = parseInt(prompt("Ingrese un sueldo entre 15000 y 70000"));
+		while (isNaN(sueldo) || sueldo < 15000 || sueldo > 70000) {
+			sueldo = parseInt(prompt("Error. Ingrese un sueldo entre 15000 y 70000"));
+		}
+		if (puesto == "programador") {
+			acumProgramador += sueldo
+			contProgramador++
+			if (sexo == "no binario" && sueldo >= 20000 && sueldo <= 55000) {
+				programNoBin++
+			}
+		} else if (puesto == "analista") {
+			acumAnalista += sueldo
+			contAnalista++
+		}
+		else {
+			acumQa += sueldo
+			contQa++
+		}
+
+		if (sexo == "femenino") {
+			sueldoF += sueldo
+		} else if (sexo == "masculino") {
+			sueldoM += sueldo
+		} else {
+			sueldoNB += sueldo
+		}
+
+		if (sueldoF > sueldoM && sueldoF > sueldoNB) {
+			sueldoMax = "femenino"
+		} else if (sueldoM >= sueldoF && sueldoM > sueldoNB) {
+			sueldoMax = "masculino"
+		} else {
+			sueldoMax = "no binario"
+		}
+
+
+		if (flagFemSueldo || sueldo > femSueldo) {
+			femSueldo = sueldo;
+			nombreFemMaxSueldo = nombre;
+			flagFemSueldo = 0
+		}
+
+		if (contProgramador != 0) {
+			promProgramador = acumProgramador / contProgramador
+		}
+		if (contAnalista != 0) {
+			promAnalista = acumAnalista / contAnalista
+		}
+		if (contQa != 0) {
+			promQa = acumQa / contQa
+		}
+
+
+
+
+
+
+
+
+
+
+
+		seguir = prompt("Desea seguir ingresando datos?:");
+	} while (seguir == "s");
+
+console.log("El promedio de sueldo de los Programadores es de " + promProgramador)
+console.log("El promedio de sueldo de los Analistas es de " + promAnalista)
+console.log("El promedio de sueldo de los Qa es de " + promQa)
+
+console.log("El sexo que tiene el mayor sueldo es el " + sueldoMax)
+	console.log("La mujer con mas sueldo se llama " + nombreFemMaxSueldo)
+	console.log("La cantidad de programadores no binarios con un sueldo entre 20000 y 55000 es de " + programNoBin)
+
+
+
+
+
+
+
 }
